48c41c10411c3012fe8af513c2f443cc7a99639a
Justin Green <justin.green@hobbylobby.com>
2025-04-11 15:23:03 -0500

    error examples

---
+++ node/error.js
@@ -0,0 +1,9 @@
+// try {
+//   throw new Error("Custom Error")
+// } catch(error) {
+//   console.log(error)
+// }
+
+// console.log(new Error("Custom Error").stack)
+
+console.trace(new Error("My Error"))
