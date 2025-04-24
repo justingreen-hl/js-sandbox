a207983d55eddd2f755e354895006f4c1f4904d9
Justin Green <justin.green@hobbylobby.com>
2025-04-24 10:36:58 -0500

    extend getDate method

--- checkTime.js
+++ checkTime.js
@@ -6,8 +6,24 @@ class CheckTime {
     const ampm = date.getHours() < 12 ? 'am' : 'pm';
     return `${hours}:${minutes}${ampm}`
   }
+
+  getDate() {
+    const date = new Date();
+    const month = date.getMonth();
+    const monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const day = date.getDay();
+    const daysArr = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const currentDayNumber = date.getDate();
+    return `${monthArr[month]} ${currentDayNumber}, ${date.getFullYear()} ${daysArr[day]}`;
+  }
+
+  getCurrentDayNumber() {
+    const date = new Date().getDate();
+    return date;
+  }
 }
 
 const test = new CheckTime();
 
-console.log(test.getTime());
+// console.log(test.getTime());
+console.log(test.getDate());
